f5d135cce99011235a91b33e2668cbe01ba16901
ajaysharma388 <ajaysm2201@gmail.com>
2019-09-23 02:42:48 +0530

    update-2019.09.23. 3

---
+++ variables.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+# we have two types of variable in the shell scripting 
+# a. System variables (gernally are in capital letter's).
+# b. User defined varibles (gernally are in lowercase).
+echo $BASH
+echo "This $BASH simply prints the path for the bash shell."
+
